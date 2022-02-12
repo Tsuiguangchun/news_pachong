97ebedb7660380637d56e96fc5995f3cf84f997a
徐光春 <97667878+Tsuiguangchun@users.noreply.github.com>
2022-02-12 17:32:45 +0800

    Update connect_mysql.py
    更新插入数据时有时候出现错误的问题

--- connect_mysql.py
+++ connect_mysql.py
@@ -4,44 +4,94 @@
 # @FlieName: connect_mysql.py
 # @SoftWare: PyCharm
 
+
+
 import pymysql
+from pymysql.cursors import DictCursor
 
 
-class OperationMysql:
-    def __init__(self):
-        self.conn = pymysql.connect(
+class pymysqltest:
+
+    def __init__(self):  # cursorclass=DictCursor 显示为字典
+        # 连接数据库
+        self.db = pymysql.connect(
             user='xuguangchun',
             password='test123456',
             host='localhost',
             port=3306,
             database='newsinfomation',
-            cursorclass=pymysql.cursors.DictCursor
-        )  # 查询以字典返回
-        self.cursor = self.conn.cursor()  # 初始化游标对象
+            cursorclass=pymysql.cursors.DictCursor)
+        # 创建游标对象cursor
+        self.cursor = self.db.cursor()
 
-    def find_db(self, mysql):
-        self.cursor.execute(mysql)
-        result = self.cursor.fetchall()
-        print(result)
-        return result
+    def query(self, sql, one=True):
+        # 使用execute 执行sql语句
+        self.cursor.execute(sql)
+        if one:
+            # 如果为one ，使用 fetchone() 方法获取单条数据
+            return self.cursor.fetchone()
+        else:
+            # 使用 fetchall() 方法获取所有数据
+            return self.cursor.fetchall()
 
-    def insert_db(self, mysql, values):
+    def updateAndDelete(self, sql, values):
         try:
-            self.cursor.execute(mysql, values)
-            self.conn.commit()
+            # 使用execute 执行sql语句
+            self.cursor.execute(sql, values)
+            # 提交到数据库执行
+            self.db.commit()
         except:
-            self.conn.rollback()
-        self.conn.close()
-        self.cursor.close()
+            # 如果发生错误回滚数据
+            self.db.rollback()
 
-    def delete_db(self, mysql):
-        try:
-            self.cursor.execute(mysql)
-            self.conn.commit()
-        except:
-            self.conn.rollback()    # 错误回滚
-        self.conn.close()
+    def close(self):
+        # 关闭游标
         self.cursor.close()
+        # 关闭数据库
+        self.db.close()
+        
+        
+"""
+原有问题
+# import pymysql
+
+
+# class OperationMysql:
+#     def __init__(self):
+#         self.conn = pymysql.connect(
+#             user='xuguangchun',
+#             password='test123456',
+#             host='localhost',
+#             port=3306,
+#             database='newsinfomation',
+#             cursorclass=pymysql.cursors.DictCursor
+#         )  # 查询以字典返回
+#         self.cursor = self.conn.cursor()  # 初始化游标对象
+
+#     def find_db(self, mysql):
+#         self.cursor.execute(mysql)
+#         result = self.cursor.fetchall()
+#         print(result)
+#         return result
+
+#     def insert_db(self, mysql, values):
+#         try:
+#             self.cursor.execute(mysql, values)
+#             self.conn.commit()
+#         except:
+#             self.conn.rollback()
+#         self.conn.close()
+#         self.cursor.close()
+
+#     def delete_db(self, mysql):
+#         try:
+#             self.cursor.execute(mysql)
+#             self.conn.commit()
+#         except:
+#             self.conn.rollback()    # 错误回滚
+#         self.conn.close()
+#         self.cursor.close()
+"""
 
 # if __name__ == '__main__':
 #     connect = OperationMysql()
